4de37397a2e43ee0616dad275a0342cf024aeaf5
superwit <pjwang_zzu@126.com>
2019-11-29 12:09:39 +0800

    修改工作路径

--- src/equant-script.py
+++ src/equant-script.py
@@ -4,15 +4,15 @@ import re
 import sys
 import os
 
+path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(os.path.abspath(path))
+
 import platform
 from equant import main
 from equant import excepthook_
 
 
 if __name__ == '__main__':
-    path = os.path.dirname(os.path.abspath(__file__))
-    os.chdir(os.path.abspath(path))
-
     sys.excepthook = excepthook_
     # ------------任务栏显示app的图标-----------------
     if 'Windows' == platform.system():
