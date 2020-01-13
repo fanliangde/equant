c44a37e75a7dc12811d07d366422f54f147a292b
lmx-hub <lmxeq5@qq.com>
2020-01-13 10:03:48 +0800

    update.bat修改

--- update.bat
+++ update.bat
@@ -1,14 +1,13 @@
 @echo off
 
-set curdir="%~dp0"
-set pypath="%~dp0../Miniconda/"
+set curdir=%~dp0
+set pypath=%~dp0..\Miniconda\
 
 
 if not exist %pypath% (
 	set pypath=
 )
 
-::cmd /k %pypath%python.exe %curdir%update.py %1
-cmd /k %pypath%python.exe "%~dp0\update.py"
+cmd /k %pypath%python.exe %curdir%update.py
 pause
                 
